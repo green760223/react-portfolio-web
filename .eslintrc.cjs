058d479951778028ebc3ad708017392ebb63e518
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-04-22 12:34:01 -0700

    Implement about and motion effect section

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,5 +21,6 @@ module.exports = {
     "no-unused-vars": "off",
     "react/no-unescaped-entities": "off",
     "react/no-unknown-property": "off",
+    "react-refresh/only-export-components": "off",
   },
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,27 +2,28 @@ import { Suspense, useEffect, useState } from "react"
 import { Canvas } from "@react-three/fiber"
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
 import CanvasLoader from "../Loader"
+import PropTypes from "prop-types"
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf")
 
   return (
     <mesh>
-      <hemisphereLight intensity={3} groundColor='black' />
-      <pointLight intensity={4} />
+      <hemisphereLight intensity={2} groundColor='black' />
+      <pointLight intensity={3} />
       <spotLight
         position={[-1, 3, 1]}
         angle={1}
         penumbra={1}
-        intensity={300}
+        intensity={200}
         castShadow
         shadow-mapSize={1024}
       />
 
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -30,6 +31,28 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    // Check if the user is on a mobile device
+    const mediaQuery = window.matchMedia("(max-width: 650px)")
+
+    // Set the initial state of the isMobile state
+    setIsMobile(mediaQuery.matches)
+
+    // Update the isMobile state when the user changes the viewport
+    const handleMediaQueryChange = (e) => {
+      setIsMobile(e.matches)
+    }
+
+    // Add the event listener to the mediaQuery
+    mediaQuery.addEventListener("change", handleMediaQueryChange)
+
+    // Remove the event listener when the component unmounts
+    return () =>
+      mediaQuery.removeEventListener("change", handleMediaQueryChange)
+  }, [])
+
   return (
     <Canvas
       frameloop='demand'
@@ -45,12 +68,16 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
     </Canvas>
   )
+}
+
+Computers.propTypes = {
+  isMobile: PropTypes.bool.isRequired,
 }
 
 export default ComputersCanvas
