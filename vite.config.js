9bd5d0c39ade8e7a122ccdd7680292497be476f1
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-04-28 16:12:05 -0700

    Update config

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,5 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: process.env.NODE_ENV === "production" ? "/react-portfolio-web/" : "/",
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("node_modules")) {
-            // Put all react dependencies in react-vendor
-            if (id.match(/node_modules\/react|react-dom|react-router-dom/)) {
-              return "react-vendor"
-            }
-            // Put all other dependencies in vendor
-            return "vendor"
-          }
-        },
-      },
-    },
-  },
+  base: "/",
 })
